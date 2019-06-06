e3779baf92829cfbda56669ad3f48c3d8b13f45b
User <User@User-PC>
2019-06-07 00:46:04 +0300

    2

--- src/main/java/ua/dimson/App.java
+++ src/main/java/ua/dimson/App.java
@@ -4,10 +4,12 @@ package ua.dimson;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+
+		int i;
+		int b;
+
+		System.out.println("Hello World!");
+	}
 }
